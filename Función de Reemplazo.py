ea8e91bf527b11cc445ec4e33f88db21857ffdd1
Mals12 <zS170133@estudiantes.uv.mx>
2021-04-09 14:16:30 -0500

    se modifico funcion de reemplazo

--- Función de Reemplazo.py
+++
@@ -1,3 +0,0 @@
-def reem_generacional(pobla_mutada):
-    poblacion=pobla_mutada
-    return poblacion
---
+++ funcion_reemplazo.py
@@ -0,0 +1,3 @@
+def reem_generacional(pobla_mutada):
+    pobla_inicial=pobla_mutada
+    return pobla_inicial
