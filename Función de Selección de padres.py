106576e8acdca86ffe5184903f6d6b88a3f570ec
Mals12 <zS170133@estudiantes.uv.mx>
2021-04-08 20:37:34 -0500

    Se modificó la función de selección de padres.

--- Función de Selección de padres.py
+++ Función de Selección de padres.py
@@ -1,22 +1,11 @@
 def selec_padres(poblacion):
-    fittotal=0
-    pro_selec=[]
-    val_esp=[]
     padres=[]
-    for i in range(0,25):
-        for i in range(len(poblacion)):
-            fittotal+=apt_fit(poblacion[i])
-        for i in range(len(poblacion)):
-            proselec=apt_fit(poblacion[i])/fittotal
-            pro_selec.append(proselec)
-            valesp=pro_selec[i]*50
-            val_esp.append(valesp)
-            ele_max=max(pro_selec)
-            pos_pro_selec=pro_selec.index(ele_max)
-            papa1=poblacion[pos_pro_selec]
-            ele_min=min(val_esp)
-            pos_val_esp=val_esp.index(ele_min)
-            papa2=poblacion[pos_val_esp]
+    lisvacia=poblacion.copy()
+    while len(lisvacia)>0:
+        papa1=rm.choice(lisvacia)
+        lisvacia.remove(papa1)
+        papa2=rm.choice(lisvacia)
+        lisvacia.remove(papa2)
         padres.append(papa1)
         padres.append(papa2)
     return padres
