b1ff9cd3a5b4334a5f97a7a238b24802bb78a9e0
Mals12 <zS170133@estudiantes.uv.mx>
2021-04-21 12:51:42 -0500

    Se modificó la función.

--- funcion_seleccion_padres.py
+++ funcion_seleccion_padres.py
@@ -1,12 +1,25 @@
 import random as rm
 def selec_padres(poblacion):
-    padres=[]
-    lisvacia=poblacion.copy()
-    while len(lisvacia)>0:
-        papa1=rm.choice(lisvacia)
-        lisvacia.remove(papa1)
-        papa2=rm.choice(lisvacia)
-        lisvacia.remove(papa2)
-        padres.append(papa1)
-        padres.append(papa2)
-    return padres
+    pro_sel=[]
+    pro_acu=[]
+    pobla_padres=[]
+    pobla_orden=sorted(poblacion,key=apt_fit)
+    for i in range(1,len(poblacion)+1):
+        prosel=(len(poblacion)-i+1)/((len(poblacion)*(len(poblacion)+1))/2)
+        pro_sel.append(prosel)
+        proacu=sum(pro_sel)
+        pro_acu.append(proacu)
+    while len(pobla_padres)<len(poblacion):
+        r_1=random.random()
+        r_2=random.random()
+        c_1=0
+        c_2=0
+        while pro_acu[c_1]<r_1:
+            c_1+=1
+        while pro_acu[c_2]<r_2:
+            c_2+=1
+        papa_1=pobla_orden[c_1]
+        papa_2=pobla_orden[c_2]
+        pobla_padres.append(papa_1)
+        pobla_padres.append(papa_2)
+    return pobla_padres
